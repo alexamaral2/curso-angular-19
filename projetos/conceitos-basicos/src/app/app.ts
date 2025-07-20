c74e86919c5d3dee90061d50ef149d50246733e1
Alex Junior <alexjunior2018@gmail.com>
2025-07-20 13:37:23 -0300

    adicionando calculadora

--- projetos/conceitos-basicos/src/app/app.ts
+++ projetos/conceitos-basicos/src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HelloWorldComponent } from './helloworld/helloworld.component'
-import { Minhapagina } from './minhapagina/minhapagina'
+import { CalculadoraComponent } from './calculadora/calculadora'
 
 @Component({
   selector: 'app-root',
-  imports: [HelloWorldComponent, Minhapagina],
+  imports: [ CalculadoraComponent ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

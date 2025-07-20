d12f86de74235b5b53e0946c5cd830845011d96e
Alex Junior <alexjunior2018@gmail.com>
2025-07-20 12:50:21 -0300

    initial commit

--- projetos/conceitos-basicos/src/app/app.ts
+++ projetos/conceitos-basicos/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from './helloworld/helloworld.component'
+import { Minhapagina } from './minhapagina/minhapagina'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HelloWorldComponent, Minhapagina],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

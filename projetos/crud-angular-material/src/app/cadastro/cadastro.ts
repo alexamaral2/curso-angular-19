4202e4d163ffc51b0e4b70e2383bb6030c339d34
Alex Junior <alexjunior2018@gmail.com>
2025-07-28 12:42:37 -0300

    adicionando exercicio

--- projetos/crud-angular-material/src/app/cadastro/cadastro.ts
+++ projetos/crud-angular-material/src/app/cadastro/cadastro.ts
@@ -7,6 +7,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { Cliente } from './cliente';
+import {ClienteService} from '../cliente.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -25,7 +26,10 @@ import { Cliente } from './cliente';
 export class Cadastro {
   cliente: Cliente = Cliente.newCliente();
 
+  constructor(private service : ClienteService){
+  }
+
   salvar(){
-    console.log("Dados do Cliente", this.cliente);
+    this.service.salvar(this.cliente);
   }
 }

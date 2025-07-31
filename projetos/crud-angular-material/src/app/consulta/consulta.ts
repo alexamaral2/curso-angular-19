dc58d950b52931a22da37e1e7e4e6b688590be2b
Alex Junior <alexjunior2018@gmail.com>
2025-07-31 14:13:08 -0300

    adicionando exercicio

--- projetos/crud-angular-material/src/app/consulta/consulta.ts
+++ projetos/crud-angular-material/src/app/consulta/consulta.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {FormsModule} from '@angular/forms';
+import {MatTableModule} from '@angular/material/table';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
+import {ClienteService} from '../cliente.service';
+import {Cliente} from '../cadastro/cliente';
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    FormsModule,
+    MatTableModule,
+    FlexLayoutModule,
+    MatButtonModule,
+  ],
   templateUrl: './consulta.html',
   styleUrl: './consulta.scss'
 })
-export class Consulta {
+export class Consulta implements OnInit {
 
+  listaClientes: Cliente[] = [];
+  constructor(private service: ClienteService) {
+  }
+
+  ngOnInit(){
+    this.listaClientes = this.service.pesquisarClientes('');
+  }
 }

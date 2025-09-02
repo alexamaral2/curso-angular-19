9c3a2d25be21d5a7339d3e27d1389e994891293f
Alex Junior <alexjunior2018@gmail.com>
2025-09-02 16:42:26 -0300

    adicionando formulario

--- projetos/passeio-app/src/app/categorias/categoria/categoria.ts
+++ projetos/passeio-app/src/app/categorias/categoria/categoria.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-categoria',
@@ -7,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrl: './categoria.scss'
 })
 export class Categoria {
+  camposForm: FormGroup;
 
+  constructor() {
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+      descricao: new FormControl('', [Validators.required]),
+    });
+  }
+
+  salvar(){
+    this.camposForm.markAllAsTouched();
+    if(this.camposForm.valid){
+      console.log('valores digitados: ', this.camposForm.value);
+    }
+  }
+
+  isCampoInvalido(nomeCampo : string){
+    const campo = this.camposForm.get(nomeCampo);
+    return campo?.invalid && campo?.touched && campo?.hasError('required');
+  }
 }
